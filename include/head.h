e8c18392f66dd9778cd68ca630f93fc4a60d17f2
wangqc_ <472018166@qq.com>
2021-05-08 01:06:26 -0700

    增加算法头文件

---
+++ include/head.h
@@ -0,0 +1,28 @@
+#ifndef HEAD_H
+#define HEAD_H
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <limits.h>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+typedef unsigned long long ull;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class ArrayReader {
+public:
+    int get(int index);
+};
+#endif
