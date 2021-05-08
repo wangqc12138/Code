19fd7100d0feb26440b326d92c1c8a949f93677a
wangqc_ <472018166@qq.com>
2021-05-08 02:29:20 -0700

    修改部分SC代码

---
+++ Server_Client/include/head.h
@@ -0,0 +1,10 @@
+#ifndef HEAD_H
+#define HEAD_H
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+using namespace std;
+#endif
