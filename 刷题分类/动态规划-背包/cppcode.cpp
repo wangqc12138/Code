eab20ce02444f37fc1cd85b36d1754dec9b9d40e
wangqc_ <472018166@qq.com>
2021-05-10 20:40:30 -0700

    新增3题背包问题

--- 刷题分类/动态规划-背包/cppcode.cpp
+++ 刷题分类/动态规划-背包/cppcode.cpp
@@ -76,3 +76,84 @@ public:
 		return sum-2*dp[target];
     }
 };
+/* 
+给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+给你一个整数 n ，返回和为 n 的完全平方数的 最少数量 。
+完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。
+例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+279
+ */
+class Solution {
+public:
+    int numSquares(int n) {
+		vector<int> nums;
+		for(int i=1;i<=n;i++){
+			if(i*i<n){
+				nums.push_back(i*i);
+			}else if(i*i==n){
+				return 1;
+			}else{
+				break;
+			}
+		}
+		vector<int> dp(n+1,n+1);
+		dp[0]=0;
+		for(int num:nums){
+			for(int i=num;i<=n;i++){
+				dp[i]=min(dp[i-num]+1,dp[i]);
+			}
+		}
+		return dp[n];
+    }
+};
+//True/Fale问题
+/* 
+给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+416
+ */
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+		int sum=accumulate(nums.begin(),nums.end(),0);
+		if(sum%2!=0){
+			return false;
+		}
+		int target=sum/2;
+		vector<bool> dp(target+1,false);
+		dp[0]=true;
+		for(int num:nums){
+			for(int i=target;i>=num;i--){
+				dp[i]=dp[i-num]||dp[i];
+			}
+		}
+		return dp[target];
+    }
+};
+/* 
+给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+说明：
+    拆分时可以重复使用字典中的单词。
+    你可以假设字典中没有重复的单词。
+139
+ */
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+		vector<bool> dp(s.size()+1,false);
+		dp[0]=true;
+		for(int i=1;i<dp.size();i++){
+			for(string str:wordDict){
+				string ss=s.substr(0,i);
+                int index=0;
+                int pos=ss.find(str,index);
+				while(pos!=string::npos){
+					dp[pos+str.size()]=dp[pos]||dp[pos+str.size()];
+					index=pos+1;
+                    pos=ss.find(str,index);
+				}
+			}
+		}
+		return dp[s.size()];
+    }
+};
+
