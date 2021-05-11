f897f8acc567ce0874d45d2b1c63112cca22e202
wangqc_ <472018166@qq.com>
2021-05-10 20:40:11 -0700

    新增每日一题20210511

--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -625,3 +625,28 @@ public:
 		getleaf(root->right,vec);
 	}
 };
+/* 
+给你一个整数数组 perm ，它是前 n 个正整数的排列，且 n 是个 奇数 。
+它被加密成另一个长度为 n - 1 的整数数组 encoded ，满足 encoded[i] = perm[i] XOR perm[i + 1] 。
+比方说，如果 perm = [1,3,2] ，那么 encoded = [2,1] 。
+给你 encoded 数组，请你返回原始数组 perm 。题目保证答案存在且唯一。
+1734
+ */
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded) {
+		int n=encoded.size()+1;
+		int t;
+		for(int i=1;i<=n;i++){
+			t^=i;
+		}
+		for(int i=1;i<n-1;i+=2){
+			t^=encoded[i];
+		}
+		vector<int> res(n,t);
+		for(int i=1;i<n;i++){
+			res[i]=encoded[i-1]^res[i-1];
+		}
+		return res;
+    }
+};
