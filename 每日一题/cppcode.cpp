0ac83524eca3f8ea20c64752212426644b8688e8
wangqc_ <472018166@qq.com>
2021-05-12 23:19:28 -0700

    新增20210513每日一题

--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -677,3 +677,31 @@ public:
         return res;
     }
 };
+/* 
+有一个长度为 arrLen 的数组，开始有一个指针在索引 0 处。
+每一步操作中，你可以将指针向左或向右移动 1 步，或者停在原地（指针不能被移动到数组范围外）。
+给你两个整数 steps 和 arrLen ，请你计算并返回：在恰好执行 steps 次操作以后，指针仍然指向索引 0 处的方案数。
+由于答案可能会很大，请返回方案数 模 10^9 + 7 后的结果。
+1269
+ */
+class Solution {
+public:
+    int numWays(int steps, int arrLen) {
+		int maxLen=min(steps,arrLen-1);
+		vector<vector<int>> dp(steps+1,vector<int>(maxLen+1));
+		dp[0][0]=1;
+		for(int i=1;i<=steps;i++){
+			for(int j=0;j<=maxLen;j++){
+				if(j==0){
+					dp[i][j]=dp[i-1][j]+dp[i-1][j+1];
+				}else if(j==arrLen-1){
+					dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+				}else{
+					dp[i][j]=dp[i-1][j-1]+dp[i-1][j]+dp[i-1][j+1];
+				}
+				
+			}
+		}
+		return dp[steps][0];
+    }
+};
