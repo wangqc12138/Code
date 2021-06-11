e408ee77bbb88c1905f17ff9697afabc91e46229
wangqc_ <472018166@qq.com>
2021-06-11 17:50:14 +0800

    深度搜索添加二叉树的几道题

--- 刷题分类/深度搜索/cppcode.cpp
+++ 刷题分类/深度搜索/cppcode.cpp
@@ -360,15 +360,243 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, vector<TreeNode*> &nodes) {
 		unordered_set<TreeNode*> uset(nodes.begin(),nodes.end());
-
+		return dfs(root,uset);
     }
 	TreeNode* dfs(TreeNode* root,unordered_set<TreeNode*> uset){
 		if(uset.count(root)){
 			return root;
 		}
 		if(root){
-			
+			auto l=dfs(root->left,uset);
+			auto r=dfs(root->right,uset);
+			if(l&&r){
+				return root;
+			}else if(l){
+				return l;
+			}else if(r){
+				return r;
+			}
 		}
 		return nullptr;
 	}
+};
+/* 
+根据一棵树的前序遍历与中序遍历构造二叉树。
+注意:
+你可以假设树中没有重复的元素。
+105
+前序遍历是VLR,中序遍历是LVR
+由此可以确定根节点和左部分，右部分
+改进：可以用map记录每个值在中序遍历的位置，更快
+还有迭代的做法----------
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		int n=preorder.size()-1;
+		return dfs(preorder,inorder,0,n,0,n);
+    }
+	TreeNode* dfs(vector<int>& preorder, vector<int>& inorder,int pre_b,int pre_e,int in_b,int in_e){
+		if(pre_b>pre_e||in_b>in_e){
+			return nullptr;
+		}
+		TreeNode* root=new TreeNode(preorder[pre_b]);
+		int i=in_b;
+		for(;i<inorder.size();i++){
+			if(inorder[i]==preorder[pre_b]){
+				break;
+			}
+		}
+		int len=i-in_b;
+		root->left=dfs(preorder,inorder,pre_b+1,pre_b+len,in_b,i-1);
+		root->right=dfs(preorder,inorder,pre_b+len+1,pre_e,i+1,in_e);
+		return root;
+	}
+};
+/* 
+根据一棵树的中序遍历与后序遍历构造二叉树。
+
+注意:
+你可以假设树中没有重复的元素。
+后序为LRV
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+		int n=inorder.size();
+		for(int i=0;i<n;i++){
+			in[inorder[i]]=i;
+		}
+		return dfs(inorder,postorder,0,n-1,0,n-1);
+    }
+	TreeNode* dfs(vector<int>& inorder, vector<int>& postorder,int in_b,int in_e,int post_b,int post_e){
+		if(in_b>in_e||post_b>post_e){
+			return nullptr;
+		}
+		TreeNode* root=new TreeNode(postorder[post_e]);
+		int inv=in[postorder[post_e]];
+		root->left=dfs(inorder,postorder,in_b,inv-1,post_b,post_b+inv-in_b-1);
+		root->right=dfs(inorder,postorder,inv+1,in_e,post_b+inv-in_b,post_e-1);
+		return root;
+	}
+private:
+	unordered_map<int,int> in;
+};
+/* 
+返回与给定的前序和后序遍历匹配的任何二叉树。
+
+ pre 和 post 遍历中的值是不同的正整数。
+ 889
+ 前序VLR,后序LRV
+ 前序遍历的头和后序遍历的尾相同；则左子树的前的左子树的尾，所以L的长度为后L的尾-前L的头,可以知道L的长度
+ */
+class Solution {
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+		int n=preorder.size();
+		for(int i=0;i<n;i++){
+			pre[preorder[i]]=i;
+			post[postorder[i]]=i;
+		}
+		return dfs(preorder,postorder,0,n-1,0,n-1);
+    }
+	TreeNode* dfs(vector<int>& preorder, vector<int>& postorder,int pre_b,int pre_e,int post_b,int post_e){
+		if(pre_b>pre_e||post_b>post_e){
+			return nullptr;
+		}
+		TreeNode* root=new TreeNode(preorder[pre_b]);
+		if(pre_b==pre_e||post_b==post_e){
+			return root;
+		}
+		int len=post[preorder[pre_b+1]]-post_b;//左子树的长度
+		root->left=dfs(preorder,postorder,pre_b+1,pre_b+1+len,post_b,post_b+len);
+		root->right=dfs(preorder,postorder,pre_b+1+len+1,pre_e,post_b+len+1,post_e-1);
+		return root;
+	}
+private:
+	unordered_map<int,int> pre,post;
+};
+/* 
+返回与给定前序遍历 preorder 相匹配的二叉搜索树（binary search tree）的根结点。
+
+(回想一下，二叉搜索树是二叉树的一种，其每个节点都满足以下规则，对于 node.left 的任何后代，值总 < node.val，
+而 node.right 的任何后代，值总 > node.val。此外，前序遍历首先显示节点 node 的值，然后遍历 node.left，接着遍历 node.right。）
+
+题目保证，对于给定的测试用例，总能找到满足要求的二叉搜索树。
+1008
+前序遍历VLR，对应二叉搜索树中小大
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+		return dfs(preorder,0,preorder.size()-1);
+    }
+	TreeNode* dfs(vector<int>& preorder,int pre_b,int pre_e){
+		if(pre_b>pre_e){
+			return nullptr;
+		}
+		TreeNode* root=new TreeNode(preorder[pre_b]);
+		if(pre_b==pre_e){
+			return root;
+		}
+		int i=pre_b;
+		for(;i<=pre_e;i++){
+			if(preorder[i]>preorder[pre_b]){
+				break;
+			}
+		}
+		root->left=dfs(preorder,pre_b+1,i-1);
+		root->right=dfs(preorder,i,pre_e);
+		return root;
+	}
+};
+/* 
+给定一个整数数组，你需要验证它是否是一个二叉搜索树正确的先序遍历序列。
+
+你可以假定该序列中的数都是不相同的。
+255
+可以从上题中获取一些信息，当找到右子树时，右子树必大于根，所以当根大于右子树的任一节点时，返回false;超时！
+
+本题也可以用单调栈----------
+ */
+class Solution {
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+		return dfs(preorder,0,preorder.size()-1,INT_MIN,INT_MAX);
+    }
+	bool dfs(vector<int>& preorder,int pre_b,int pre_e,int minval,int maxval){
+		if(pre_b>pre_e){
+			return true;
+		}
+		if(preorder[pre_b]<=minval||preorder[pre_b]>=maxval){
+			return false;
+		}
+		int i=pre_b+1;
+		for(;i<=pre_e;i++){
+			if(preorder[i]>=preorder[pre_b]){
+				break;
+			}
+		}
+		return dfs(preorder,pre_b+1,i-1,minval,preorder[pre_b])&&dfs(preorder,i,pre_e,preorder[pre_b],maxval);
+	}
+};
+/* 
+我们从二叉树的根节点 root 开始进行深度优先搜索。
+
+在遍历中的每个节点处，我们输出 D 条短划线（其中 D 是该节点的深度），然后输出该节点的值。
+（如果节点的深度为 D，则其直接子节点的深度为 D + 1。根节点的深度为 0）。
+
+如果节点只有一个子节点，那么保证该子节点为左子节点。
+
+给出遍历输出 S，还原树并返回其根节点 root。
+1028
+与前面几题类似！
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+		int k=0,index=0,n=traversal.size();
+		vector<int> preorder;
+		for(int i=0;i<traversal.size();i++){
+			if(traversal[i]=='-'){
+				k++;
+			}else{
+				int num=0;
+				while(i<n&&traversal[i]!='-'){
+					num=num*10+traversal[i]-'0';
+					i++;
+				}
+				dep.emplace_back(k);
+				preorder.emplace_back(num);
+				k=0;
+				i--;
+			}
+		}
+		return dfs(preorder,0,preorder.size()-1);
+    }
+	TreeNode* dfs(vector<int> preorder,int pre_b,int pre_e){
+		if(pre_b>pre_e){
+			return nullptr;
+		}
+		TreeNode* root=new TreeNode(preorder[pre_b]);
+		int rd=dep[pre_b],l=-1,r=-1;
+		for(int i=pre_b;i<=pre_e;i++){
+			if(dep[i]==rd+1){
+				if(l==-1){
+					l=i;
+				}else{
+					r=i;
+				}
+			}
+		}
+		if(r!=-1){
+            root->left=dfs(preorder,l,r-1);
+			root->right=dfs(preorder,r,pre_e);
+		}else{
+            root->left=dfs(preorder,pre_b+1,pre_e);
+        }
+		return root;
+	}
+private:
+	vector<int> dep;
 };
