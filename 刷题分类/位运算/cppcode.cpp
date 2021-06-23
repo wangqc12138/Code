79edc5ff83bf38a57f746b902a410fd3d6330f42
wangqc_ <472018166@qq.com>
2021-06-23 15:35:07 +0800

    20210623

--- 刷题分类/位运算/cppcode.cpp
+++ 刷题分类/位运算/cppcode.cpp
@@ -1,3 +1,4 @@
+#include "head.h"
 /*
 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回
 338
@@ -6,3 +7,15 @@
 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
 191
 */
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+		int res=0;
+		for(int i=0;i<31;i++){
+			if((n>>i)&&1){
+				res++;
+			}
+		}
+		return res;
+    }
+};
--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -1470,3 +1470,19 @@ public:
 		}
 	}
 };
+/*
+编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+191
+*/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+		int res=0;
+		for(int i=0;i<31;i++){
+			if((n>>i)&&1){
+				res++;
+			}
+		}
+		return res;
+    }
+};
