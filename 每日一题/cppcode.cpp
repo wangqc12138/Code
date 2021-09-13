b6891ece69650791839da3e2a30b200ac30b388d
wangqc_ <472018166@qq.com>
2021-09-12 20:37:37 -0700

    每日一题-20210913

--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -2558,3 +2558,33 @@ public:
 		return N%2==0?y*y:y*y*x;
 	}
 };
+/* 
+给定平面上 n 对 互不相同 的点 points ，其中 points[i] = [xi, yi] 。回旋镖 是由点 (i, j, k) 表示的元组 ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+
+返回平面上所有回旋镖的数量。
+447
+ */
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        if(points.size()<3){
+			return 0;
+		}
+		int res=0;
+		int n=points.size();
+		map<int,int> mp;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				int len=pow(points[i][0]-points[j][0],2)+pow(points[i][1]-points[j][1],2);
+				mp[len]++;
+			}
+			for(auto [i,j]:mp){
+				if(j>=2){
+					res+=(j-1)*j;
+				}
+			}
+			mp.clear();
+		}
+		return res;
+    }
+};
