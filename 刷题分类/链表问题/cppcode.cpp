c85d5fea4a98c1baed57d0a10bcb72038a6d2799
wangqc_ <472018166@qq.com>
2021-09-17 03:15:09 -0700

    20210917

--- 刷题分类/链表问题/cppcode.cpp
+++ 刷题分类/链表问题/cppcode.cpp
@@ -1,3 +1,4 @@
+#include "head.h"
 /*
 给你单链表的头节点 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。 
 92
@@ -61,5 +62,91 @@ public:
             
         }
         return dum->next;
+    }
+};
+/* 
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+进阶：
+
+    你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+    你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+25
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	void reverse(ListNode* head,int i,int k){
+		ListNode* pre=head;
+		for(int index=0;index<i&&pre!=nullptr;i++){
+			pre=pre->next;
+		}
+		ListNode* ptr=pre->next;
+		while(ptr->next&&k-->0){
+			ListNode *nxt=ptr->next;
+			ptr->next=nxt->next;
+			nxt->next=pre->next;
+			pre->next=nxt;
+		}
+	}
+    ListNode* reverseKGroup(ListNode* head, int k) {
+		ListNode* dum=new ListNode(0);
+		dum->next=head;
+		int i=0,j=0;
+		while(head){
+			head=head->next;
+			i++;
+			if(i-j==k){
+				j=i;
+				reverse(dum,j,k);
+			}
+		}
+		return dum->next;
+    }
+};
+/* 
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+206
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+		if(head==nullptr){
+			return nullptr;
+		}
+		ListNode* dum=new ListNode(0);
+		dum->next=head;
+		while(head->next){
+			ListNode* nxt=head->next;
+			head->next=nxt->next;
+			nxt->next=dum->next;
+			dum->next=nxt;
+		}
+		return dum->next;
     }
 };
--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -2679,3 +2679,44 @@ public:
 		return res;
     }
 };
+/* 
+请你判断一个 9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+
+    数字 1-9 在每一行只能出现一次。
+    数字 1-9 在每一列只能出现一次。
+    数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+
+数独部分空格内已填入了数字，空白格用 '.' 表示。
+
+注意：
+
+    一个有效的数独（部分已被填充）不一定是可解的。
+    只需要根据以上规则，验证已经填入的数字是否有效即可。
+36
+*/
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+		int m=board.size(),n=board[0].size();
+		vector<set<int>> vs(10); 
+		for(int i=0;i<9;i++){
+            set<char> s1,s2;
+			for(int j=0;j<9;j++){
+				int index=(i/3)*3+j/3;
+				if(board[i][j]!='.'&&vs[index].count(board[i][j])){
+					return false;
+				}
+				vs[index].emplace(board[i][j]);
+                if(board[i][j]!='.'&&s1.count(board[i][j])){
+					return false;
+				}
+				s1.emplace(board[i][j]);
+				if(board[j][i]!='.'&&s2.count(board[j][i])){
+					return false;
+				}
+				s2.emplace(board[j][i]);
+			}
+		}
+		return true;
+    }
+};
