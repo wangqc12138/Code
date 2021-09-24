e63d878b9383d57399e5d2603ed8ba030ddda3ec
wangqc_ <472018166@qq.com>
2021-09-24 01:19:38 -0700

    20210924

--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -2766,3 +2766,50 @@ public:
 		return res;
     }
 };
+/* 
+多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。
+这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
+
+给你位于列表第一级的头节点，请你扁平化列表，使所有结点出现在单级双链表中。
+430
+ */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node* p=head;
+		stack<Node*> n;
+		while(p){
+			if(p->child){
+				n.emplace(p->next);
+				p->next=p->child;
+				p->child=nullptr;
+			}
+			if(p->next==nullptr){
+				if(n.size()){
+					Node* tmp=n.top();
+					n.pop();
+					p->next=tmp;
+				}
+			}
+			p=p->next;
+		}
+		return head;
+    }
+};
