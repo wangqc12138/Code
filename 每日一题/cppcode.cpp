8a6b80ca17afa7ff7bfc60b9e362d8e0db3d43a8
wangqc_ <472018166@qq.com>
2021-09-27 01:37:13 -0700

    20210927

--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -2831,3 +2831,92 @@ public:
 		return a;
     }
 };
+/* 
+一条包含字母 A-Z 的消息通过以下的方式进行了编码：
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+
+要 解码 一条已编码的消息，所有的数字都必须分组，然后按原来的编码方案反向映射回字母（可能存在多种方式）。例如，"11106" 可以映射为：
+
+    "AAJF" 对应分组 (1 1 10 6)
+    "KJF" 对应分组 (11 10 6)
+
+注意，像 (1 11 06) 这样的分组是无效的，因为 "06" 不可以映射为 'F' ，因为 "6" 与 "06" 不同。
+
+除了 上面描述的数字字母映射方案，编码消息中可能包含 '*' 字符，可以表示从 '1' 到 '9' 的任一数字（不包括 '0'）。
+例如，编码字符串 "1*" 可以表示 "11"、"12"、"13"、"14"、"15"、"16"、"17"、"18" 或 "19" 中的任意一条消息。
+对 "1*" 进行解码，相当于解码该字符串可以表示的任何编码消息。
+
+给你一个字符串 s ，由数字和 '*' 字符组成，返回 解码 该字符串的方法 数目 。
+
+由于答案数目可能非常大，返回对 109 + 7 取余 的结果。
+639
+// 大概率回溯
+回溯大概率超时
+ */
+class Solution {
+public:
+	long long res=0,temp=1;
+    int numDecodings(string s) {
+		dfs(s,0);
+		return res;
+    }
+	void dfs(string s,int index){
+        //temp%=1000000007;
+		if(index>=s.size()){
+            //cout<<temp<<endl;
+            res%=1000000007;
+			res+=temp;
+			return;
+		}
+		if(s[index]=='1'){
+			dfs(s,index+1);
+			if(index!=s.size()-1){
+				if(s[index+1]=='*'){
+					temp*=9;
+					dfs(s,index+2);
+					temp/=9;
+				}else{
+					dfs(s,index+2);
+				}
+			}
+		}else if(s[index]=='2'){
+			dfs(s,index+1);
+			if(index!=s.size()-1){
+				if(s[index+1]=='*'){
+					temp*=6;
+					dfs(s,index+2);
+					temp/=6;
+				}else if(s[index+1]-'0'<=6){
+					dfs(s,index+2);
+				}
+			}
+		}else if(s[index]=='*'){
+			temp*=9;
+			dfs(s,index+1);
+			temp/=9;
+			if(index!=s.size()-1){
+				if(s[index+1]!='*'){
+					if(s[index+1]-'0'<=6){
+						temp*=2;
+						dfs(s,index+2);
+						temp/=2;
+					}else{
+						dfs(s,index+2);
+					}
+				}else{
+					temp*=15;
+					dfs(s,index+2);
+					temp/=15;
+				}
+			}
+		}else if(s[index]=='0'){
+			return;
+		}else{
+			dfs(s,index+1);
+		}
+	}
+};
