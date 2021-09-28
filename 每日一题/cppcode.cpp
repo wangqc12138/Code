56a26ee55d2bf75b983fdc620e6f6abe7e1580aa
wangqc_ <472018166@qq.com>
2021-09-27 19:45:33 -0700

    20210928

--- 每日一题/cppcode.cpp
+++ 每日一题/cppcode.cpp
@@ -2920,3 +2920,49 @@ public:
 		}
 	}
 };
+
+/* 
+给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+
+路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+437
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> prefix;
+
+    int dfs(TreeNode *root, long long curr, int targetSum) {
+        if (!root) {
+            return 0;
+        }
+
+        int ret = 0;
+        curr += root->val;
+        if (prefix.count(curr - targetSum)) {
+            ret = prefix[curr - targetSum];
+        }
+
+        prefix[curr]++;
+        ret += dfs(root->left, curr, targetSum);
+        ret += dfs(root->right, curr, targetSum);
+        prefix[curr]--;
+
+        return ret;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        prefix[0] = 1;
+        return dfs(root, 0, targetSum);
+    }
+};
