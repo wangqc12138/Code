6a1a63918d91550cfaac595923504afcd9a88e0b
wangqc_ <472018166@qq.com>
2021-12-17 01:58:17 -0800

    20211217

--- 随缘刷/cppcode.cpp
+++ 随缘刷/cppcode.cpp
@@ -71,3 +71,28 @@ public:
 		dfs(root->right,temp);   
 	}
 };
+/* 
+给你一个长度为 n 、下标从 1 开始的二进制字符串，所有位最开始都是 0 。我们会按步翻转该二进制字符串的所有位（即，将 0 变为 1）。
+
+给你一个下标从 1 开始的整数数组 flips ，其中 flips[i] 表示对应下标 i 的位将会在第 i 步翻转。
+
+二进制字符串 前缀一致 需满足：在第 i 步之后，在 闭 区间 [1, i] 内的所有位都是 1 ，而其他位都是 0 。
+
+返回二进制字符串在翻转过程中 前缀一致 的次数。
+1375
+ */
+class Solution {
+public:
+    int numTimesAllBlue(vector<int>& flips) {
+		int n=flips.size(),index=0,res=0;
+		long long k=0,s=0;
+		for(int i=0;i<n;i++){
+			k+=flips[i];
+			s+=i+1;
+			if(k==s){
+				res++;
+			}
+		}
+		return res;
+    }
+};
