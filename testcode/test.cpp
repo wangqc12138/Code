da204599d6a77e0d9efefc5a5693317d4ad7b855
wangqc_ <472018166@qq.com>
2022-03-17 19:16:31 -0700

    20220318

--- testcode/test.cpp
+++ testcode/test.cpp
@@ -7,91 +7,111 @@
 #include <sys/types.h>
 #include <sys/stat.h> 
 #include <unistd.h>
+#include <map>
+#include <bitset>
 using namespace std;
 vector<string> ans;
 string temp;
 //递归版本回溯
-void dfs1(vector<string>& vstr,int index){
-	if(index==vstr.size()){
-		ans.emplace_back(temp);
-		return;
-	}
-	for(int i=0;i<vstr[index].size();i++){
-		temp+=vstr[index][i];
-		dfs1(vstr,index+1);
-		temp.pop_back();
-	}
-}
-//非递归版本 BFS
-void bfs(vector<string>& vstr){
-	queue<string> mq;
-	int index=0;
-	//加入一个假定的头-空字符串
-	mq.emplace("");
-	while(index<vstr.size()){
-		int len=mq.size();
-		//将队列的字符串弹出加index层的元素
-		for(int i=0;i<len;i++){
-			auto f=mq.front();
-			mq.pop();
-			//再将拼装后的加入队列
-			for(int j=0;j<vstr[index].size();j++){
-				mq.emplace(f+vstr[index][j]);
+// void dfs1(vector<string>& vstr,int index){
+// 	if(index==vstr.size()){
+// 		ans.emplace_back(temp);
+// 		return;
+// 	}
+// 	for(int i=0;i<vstr[index].size();i++){
+// 		temp+=vstr[index][i];
+// 		dfs1(vstr,index+1);
+// 		temp.pop_back();
+// 	}
+// }
+// //非递归版本 BFS
+// void bfs(vector<string>& vstr){
+// 	queue<string> mq;
+// 	int index=0;
+// 	//加入一个假定的头-空字符串
+// 	mq.emplace("");
+// 	while(index<vstr.size()){
+// 		int len=mq.size();
+// 		//将队列的字符串弹出加index层的元素
+// 		for(int i=0;i<len;i++){
+// 			auto f=mq.front();
+// 			mq.pop();
+// 			//再将拼装后的加入队列
+// 			for(int j=0;j<vstr[index].size();j++){
+// 				mq.emplace(f+vstr[index][j]);
+// 			}
+// 		}
+// 		cout<<"第"<<index<<"层:"<<endl;
+// 		queue<string> t=mq;
+// 		int k=0;
+// 		while(!t.empty()){
+// 			cout<<t.front()<<" ";
+// 			if(++k%10==0){
+// 				cout<<endl;
+// 			}
+// 			t.pop();
+// 		}
+// 		cout<<endl;
+// 		index++;
+// 	}
+// 	while(!mq.empty()){
+// 		ans.emplace_back(mq.front());
+// 		mq.pop();
+// 	}
+// } 
+// //非递归-dfs
+// void dfs2(vector<string>& vstr){
+// 	stack<pair<string,int>> msk;
+// 	msk.emplace("",0);
+// 	while(!msk.empty()){
+// 		auto psi=msk.top();
+// 		int len=psi.first.size();
+// 		int index=psi.second;
+// 		if(len==vstr.size()){
+// 			ans.emplace_back(psi.first);
+// 			msk.pop();
+// 			continue;
+// 		}
+// 		if(index==vstr[len].size()){
+// 			msk.pop();
+// 			continue;
+// 		}
+// 		psi.first+=vstr[len][index];
+// 		psi.second=0;
+// 		msk.top().second++;
+// 		msk.emplace(psi);
+// 	}
+// }
+using ll=long long;
+ll get_soncnt(vector<vector<int>>& skills, map<ll, int> dict) {
+        int n = skills.size();
+        ll ans = 0;
+        for(int i = 0; i < n; ++i){  
+			for(auto k:skills[i]){
+				cout<<k<<":";
 			}
-		}
-		cout<<"第"<<index<<"层:"<<endl;
-		queue<string> t=mq;
-		int k=0;
-		while(!t.empty()){
-			cout<<t.front()<<" ";
-			if(++k%10==0){
+			cout<<endl;
+            int l = skills[i].size();
+            for(int t = 1; t < (1 << l) - 1; ++t){
+                ll cj = 0;
+				cout<<bitset<sizeof(int)>(t)<<":"<<endl;
+                for(int j = 0; j < l; ++j){
+                    if(t & (1 << j)){
+                        cj = (cj << 10) + skills[i][j];
+						cout<<j<<" ";
+                    }
+                }
 				cout<<endl;
-			}
-			t.pop();
-		}
-		cout<<endl;
-		index++;
-	}
-	while(!mq.empty()){
-		ans.emplace_back(mq.front());
-		mq.pop();
-	}
-} 
-//非递归-dfs
-void dfs2(vector<string>& vstr){
-	stack<pair<string,int>> msk;
-	msk.emplace("",0);
-	while(!msk.empty()){
-		auto psi=msk.top();
-		int len=psi.first.size();
-		int index=psi.second;
-		if(len==vstr.size()){
-			ans.emplace_back(psi.first);
-			msk.pop();
-			continue;
-		}
-		if(index==vstr[len].size()){
-			msk.pop();
-			continue;
-		}
-		psi.first+=vstr[len][index];
-		psi.second=0;
-		msk.top().second++;
-		msk.emplace(psi);
-	}
-}
+                ans += dict[cj];
+            }
+        }
+        return ans;
+    }
 int main(){
-	char str[1024]={0};
-	char temp[1024]={0};
-	string s="12345|qwerqwer|ASDASD;";
-	if(s.back()==';'){
-		s.pop_back();
-	}
-	if(s.find(";") != string::npos){
-		cout<<s.find(";")<<endl;
-		//s = s.replace(s.find(";"),1,":");
-	}
-	cout<<s<<endl;
-	//cout<<str<<endl;
+	vector<vector<int>> skills={{1,2,3,4}};
+	map<ll,int> cnt;
+	get_soncnt(skills,cnt);
+	int k=10&1;
+	cout<<k<<endl;
 	return 0;
 }
--- 刷题分类/字典树/cppcode.cpp
+++ 刷题分类/字典树/cppcode.cpp
@@ -57,6 +57,51 @@ private:
     vector<Trie*> next;
     bool isEnd; 
 };
+/* -----------20220311------------ */
+//this指针
+class Trie {
+public:
+    Trie() {
+		next.clear();
+		next.resize(26);
+    }
+    
+    void insert(string word) {
+		Trie* node=this;
+		for(auto c:word){
+			if(node->next[c-'a']==nullptr){
+				node->next[c-'a']=new Trie();
+			}
+			node=node->next[c-'a'];
+		}
+		node->isEnd=true;
+    }
+    
+    bool search(string word) {
+		Trie* node=this;
+		for(auto c:word){
+			if(node->next[c-'a']==nullptr){
+				return false;
+			}
+			node=node->next[c-'a'];
+		}
+		return node->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+		Trie* node=this;
+		for(auto c:prefix){
+			if(node->next[c-'a']==nullptr){
+				return false;
+			}
+			node=node->next[c-'a'];
+		}
+		return true;
+    }
+private:
+	vector<Trie*> next;
+	bool isEnd=false;
+};
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie* obj = new Trie();
@@ -206,3 +251,62 @@ public:
 		return res;
     }
 };
+/* 
+设计一个 map ，满足以下几点:
+
+    字符串表示键，整数表示值
+    返回具有前缀等于给定字符串的键的值的总和
+
+实现一个 MapSum 类：
+
+    MapSum() 初始化 MapSum 对象
+    void insert(String key, int val) 插入 key-val 键值对，字符串表示键 key ，整数表示值 val 。
+	如果键 key 已经存在，那么原来的键值对 key-value 将被替代成新的键值对。
+    int sum(string prefix) 返回所有以该前缀 prefix 开头的键 key 的值的总和。
+	key 和 prefix 仅由小写英文字母组成
+677
+ */
+class MapSum {
+public:
+    MapSum():tree(26),v(0),s(0){}
+    
+    void insert(string key, int val) {
+		MapSum* node=this;
+		for(auto c:key){
+			if(node->tree[c-'a']==nullptr){
+				node->tree[c-'a']=new MapSum();
+			}
+			node=node->tree[c-'a'];
+            node->s+=val;
+        }
+		if(node->v!=0){
+			MapSum* temp=this;
+			for(auto c:key){
+				temp=temp->tree[c-'a'];
+                temp->s-=node->v;
+			}
+		}
+		node->v=val;
+    }
+    
+    int sum(string prefix) {
+		MapSum* node=this;
+		for(auto c:prefix){
+			if(node->tree[c-'a']==nullptr){
+				return 0;
+			}
+			node=node->tree[c-'a'];
+		}
+		return node->s;
+    }
+private:
+	vector<MapSum*> tree;
+	int v,s;
+};
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum* obj = new MapSum();
+ * obj->insert(key,val);
+ * int param_2 = obj->sum(prefix);
+ */
