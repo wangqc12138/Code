16aa282fe5b708444169063b34b0039d580b1524
wangqc_ <472018166@qq.com>
2022-03-24 19:08:06 -0700

    20220325

---
+++ LCP2021春季/cppcode.cpp
@@ -0,0 +1,19 @@
+#include "head.h"
+/* 
+小力将 N 个零件的报价存于数组 nums。小力预算为 target，假定小力仅购买两个零件，要求购买零件的花费不超过预算，请问他有多少种采购方案。
+
+注意：答案需要以 1e9 + 7 (1000000007) 为底取模，如：计算初始结果为：1000000008，请返回 1
+
+    2 <= nums.length <= 10^5
+    1 <= nums[i], target <= 10^5
+
+ */
+class Solution {
+public:
+    int purchasePlans(vector<int>& nums, int target) {
+		sort(nums.begin(),nums.end());
+		for(auto i:nums){
+			int k=
+		}
+    }
+};
--- 刷题分类/字典树/cppcode.cpp
+++ 刷题分类/字典树/cppcode.cpp
@@ -310,3 +310,95 @@ private:
  * obj->insert(key,val);
  * int param_2 = obj->sum(prefix);
  */
+/* 
+在英语中，我们有一个叫做 词根(root) 的概念，可以词根后面添加其他一些词组成另一个较长的单词——我们称这个词为 继承词(successor)。
+例如，词根an，跟随着单词 other(其他)，可以形成新的单词 another(另一个)。
+
+现在，给定一个由许多词根组成的词典 dictionary 和一个用空格分隔单词形成的句子 sentence。
+你需要将句子中的所有继承词用词根替换掉。如果继承词有许多可以形成它的词根，则用最短的词根替换它。
+
+你需要输出替换之后的句子。
+648
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 100
+dictionary[i] 仅由小写字母组成。
+1 <= sentence.length <= 10^6
+sentence 仅由小写字母和空格组成。
+sentence 中单词的总量在范围 [1, 1000] 内。
+sentence 中每个单词的长度在范围 [1, 1000] 内。
+sentence 中单词之间由一个空格隔开。
+sentence 没有前导或尾随空格。
+ */
+class trie{
+public:
+	trie(){
+		next.resize(26);
+		isEnd=false;
+	}
+	void add(const string& str){
+		trie* node=this;
+		for(auto c:str){
+			if(node->next[c-'a']==nullptr){
+				node->next[c-'a']=new trie();
+			}
+			node=node->next[c-'a'];
+		}
+		node->isEnd=true;
+	}
+	string find(const string& str){
+		trie* node=this;
+		string res="";
+		for(auto c:str){
+			if(node->next[c-'a']==nullptr){
+				break;
+			}
+			node=node->next[c-'a'];
+			res+=c;
+			if(node->isEnd==true){
+				return res;
+			}
+		}
+		return "";
+	}
+private:
+	vector<trie*> next;
+	bool isEnd;
+};
+class Solution {
+public:
+	int splite(string str,string x,vector<string> &vstr){
+		int index=0,n=str.size();
+		while(int k=str.find(x,index)!=string::npos){
+			vstr.emplace_back(str.substr(index,k-index+x.size()));
+			index=k+x.size();
+		}
+		return vstr.size();
+	}
+	void splite(string& data,vector<string>& vec){
+		int i=0;
+		while(i<data.size()){
+			int n=data.find(' ',i);
+			vec.emplace_back(data.substr(i,n-i));
+			i=n+1;
+		}
+	}
+    string replaceWords(vector<string>& dictionary, string sentence) {
+		vector<string> vstr;
+		splite(sentence,vstr);
+		trie Trie;
+		for(auto str:dictionary){s
+			Trie.add(str);
+		}
+		string res="";
+		for(auto str:vstr){
+			string s=Trie.find(str);
+			if(s!=""){
+				res+=s+" ";
+			}else{
+				res+=str+" ";
+			}
+		}
+		res.pop_back();
+		return res;
+    }
+};
