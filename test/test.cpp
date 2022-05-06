626a325107f20a754b4c0c84b249385f439b90aa
wangqc12138 <472018166@qq.com>
2022-05-07 00:39:32 +0800

    20220507

--- test/test.cpp
+++ test/test.cpp
@@ -3,45 +3,50 @@ using namespace std;
 using pii=pair<int,int>;
 int main(){
     freopen("./test.txt","r",stdin);
-    int M;
-    cin>>M;
-    while(M--){
-        int N;
-        cin>>N;
-        int a[3][N];
-        int b[3][N+1];
-        for(int i=0;i<3;i++)
-        for(int j=0;j<N;j++){
-            cin>>a[i][j];
-        }
-        set<int> st;
-        for(int i=0;i<N;i++){
-            if(st.count(a[1][i])||st.count(a[0][i])||a[2][i]!=0){
-                st.emplace(a[0][i]);
-                st.emplace(a[2][i]);
+    int N;
+    cin>>N;
+    while(N--){
+        int M;
+        cin>>M;
+        int son[M+1],parent[M+1],visit[M+1];
+        memset(son,1,sizeof(int)*(M+1));
+        memset(visit,0,sizeof(int)*(M+1));
+        int index=0; 
+        for(int i=0;i<M;i++){
+            int K;
+            cin>>K;
+            if(son[K]){
+                index++;
             }
-            if(a[1][i]==a[0][i]){
-                st.emplace(a[0][i]);
-            }
+            son[K]=0;
+            parent[i+1]=K;
         }
-        set<pii> sp;
-        int k=0,ans=2;
-        for(int i=0;i<N;i++){
-            if(st.count(a[0][i])){
+        if(M==1){
+            cout<<1<<endl<<1<<endl<<1<<endl<<endl;;
+            continue;
+        }
+        cout<<M-index<<endl;
+        for(int i=0;i<M;i++){
+            if(son[i+1]==0){
                 continue;
-            }else{
-                k++;
-                if(sp.count(pii(a[0][i],a[1][i]))){
-                    ans*=2;
-                    k--;
+            }
+            int k=i+1;
+            vector<int> ans;
+            while(!visit[k]){
+                ans.emplace_back(k);
+                visit[k]=1;
+                k=parent[k];
+            }
+            cout<<ans.size()<<endl;
+            for(int j=ans.size()-1;j>=0;j--){
+                if(j==0){
+                    cout<<ans[j]<<endl;
+                }else{
+                    cout<<ans[j]<<" ";
                 }
-                sp.emplace(a[1][i],a[0][i]);
             }
         }
-        if(k==sp.size()/2){
-            ans/=2;
-        }
-        cout<<ans<<endl;
+        cout<<endl;
     }
     return 0;
 }
