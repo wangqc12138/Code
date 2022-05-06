6aaa585461d20869059b5d76e96b98a3bdcd3953
wangqc12138 <472018166@qq.com>
2022-05-06 16:30:36 +0800

    20220506

--- codeforces/20220505/d.cpp
+++ codeforces/20220505/d.cpp
@@ -2,23 +2,50 @@
 using namespace std;
 int main(){
     freopen("./test.txt","r",stdin);
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int m;
-        cin>>m;
-        int root;
-        map<int,vector<int>> next;
-        for(int j=0;j<m;j++){
-            int k;
-            cin>>k;
-            if(k==j+1){
-                root=k;
-            }else{
-                next[j+1].emplace_back(k);
+    int N;
+    cin>>N;
+    while(N--){
+        int M;
+        cin>>M;
+        int son[M+1],parent[M+1],visit[M+1];
+        memset(son,1,sizeof(int)*(M+1));
+        memset(visit,0,sizeof(int)*(M+1));
+        int index=0; 
+        for(int i=0;i<M;i++){
+            int K;
+            cin>>K;
+            if(son[K]){
+                index++;
+            }
+            son[K]=0;
+            parent[i+1]=K;
+        }
+        if(M==1){
+            cout<<1<<endl<<1<<endl<<1<<endl<<endl;;
+            continue;
+        }
+        cout<<M-index<<endl;
+        for(int i=0;i<M;i++){
+            if(son[i+1]==0){
+                continue;
+            }
+            int k=i+1;
+            vector<int> ans;
+            while(!visit[k]){
+                ans.emplace_back(k);
+                visit[k]=1;
+                k=parent[k];
+            }
+            cout<<ans.size()<<endl;
+            for(int j=ans.size()-1;j>=0;j--){
+                if(j==0){
+                    cout<<ans[j]<<endl;
+                }else{
+                    cout<<ans[j]<<" ";
+                }
             }
         }
-        auto dfs=[](int)
+        cout<<endl;
     }
     return 0;
 }
