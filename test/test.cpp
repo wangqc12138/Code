cd8ee6388542947d0d90536bcd4aa957ed479ed3
wangqc12138 <472018166@qq.com>
2022-05-30 10:23:31 +0800

    20220530

--- test/test.cpp
+++ test/test.cpp
@@ -1,30 +1,61 @@
 #include <bits/stdc++.h>
+#include <sys/types.h> 
+#include <dirent.h>
 using namespace std;
-int main(){
-    freopen("./test.txt","r",stdin);
-    vector<vector<int>> m(10);
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        string temp;
-        cin>>temp;
-        int index=0;
-        for(auto c:temp){
-            m[c-'0'].emplace_back(++index);
-        }
-
+class file{
+public:
+    file(string name,string dir,int size=0):name(name),dir(dir),size(size){};
+    string name;
+    string dir;
+    int size;
+};
+class node{
+public:
+    node(string name){
+        this->name=name;
+        this->childdir.clear();
+        this->childfile.clear();
     }
-    int ans=10*(n-1);
-    for(int i=0;i<10;i++){
-        map<int,int> mp;
-        int t=0,k=0;
-        for(int j:m[i]){
-            t=max(t,j+mp[j]);
-            k=max(k,mp[j+mp[j]]++);
-        }
-        cout<<
-        ans=min(k*10+t-1,ans);
-    }
-    cout<<ans<<endl;
+    string name;
+    vector<node*> childdir;
+    vector<string> childfile;
+};
+map<string,vector<file>> mpfile;
+set<string> visit;
+int dfs(node* root,string dir){
+    DIR *dDir;
+	struct dirent *dp;
+    if ((dDir = opendir (dir.c_str ())) == NULL) {
+		return -1;
+	}
+	while((dp = readdir (dDir))){
+		if (strcmp (dp->d_name, ".") == 0 || strcmp (dp->d_name, "..") == 0 ) {
+			continue;
+		}
+		string temp=dir+"/"+dp->d_name;
+        //文件
+		if(dp->d_type==8){
+			root->childfile.emplace_back(temp);
+            mpfile[temp].emplace_back(new file(dp->d_name,dir));
+		}
+		//目录
+		if(dp->d_type==4){
+            root->childfile.emplace_back(dp->d_name);
+            node *chdir=new node(temp);
+			root->childdir.emplace_back(chdir);
+            dfs(chdir,temp);
+		}
+        
+	}
     return 0;
 }
+int printTree
+int main(){
+    string dir="./test";
+    node *root=new node(dir);
+    if(dfs(root,dir) == 0){
+        bfs(root);
+    }
+    return 0;
+}
+
