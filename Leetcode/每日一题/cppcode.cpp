6a31397b0ab69af80b3900be0f44c16ae733c0a5
wangqc12138 <472018166@qq.com>
2022-07-26 18:07:46 +0800

    20220726

--- Leetcode/每日一题/cppcode.cpp
+++ Leetcode/每日一题/cppcode.cpp
@@ -4896,3 +4896,138 @@ public:
 		return res;
     }
 };
+/* 
+不使用任何库函数，设计一个 跳表 。
+
+跳表 是在 O(log(n)) 时间内完成增加、删除、搜索操作的数据结构。跳表相比于树堆与红黑树，其功能与性能相当，并且跳表的代码长度相较下更短，其设计思想与链表相似。
+
+例如，一个跳表包含 [30, 40, 50, 60, 70, 90] ，然后增加 80、45 到跳表中，以下图的方式操作：
+1206
+ */
+constexpr int MAX_LEVEL = 32;
+constexpr double P_FACTOR = 0.25;
+
+struct SkiplistNode {
+    int val;
+    vector<SkiplistNode *> forward;
+    SkiplistNode(int _val, int _maxLevel = MAX_LEVEL) : val(_val), forward(_maxLevel, nullptr) {
+        
+    }
+};
+
+class Skiplist {
+private:
+    SkiplistNode * head;
+    int level;
+    mt19937 gen{random_device{}()};
+    uniform_real_distribution<double> dis;
+
+public:
+    Skiplist(): head(new SkiplistNode(-1)), level(0), dis(0, 1) {
+
+    }
+
+    bool search(int target) {
+        SkiplistNode *curr = this->head;
+        for (int i = level - 1; i >= 0; i--) {
+            /* 找到第 i 层小于且最接近 target 的元素*/
+            while (curr->forward[i] && curr->forward[i]->val < target) {
+                curr = curr->forward[i];
+            }
+        }
+        curr = curr->forward[0];
+        /* 检测当前元素的值是否等于 target */
+        if (curr && curr->val == target) {
+            return true;
+        } 
+        return false;
+    }
+
+    void add(int num) {
+        vector<SkiplistNode *> update(MAX_LEVEL, head);
+        SkiplistNode *curr = this->head;
+        for (int i = level - 1; i >= 0; i--) {
+            /* 找到第 i 层小于且最接近 num 的元素*/
+            while (curr->forward[i] && curr->forward[i]->val < num) {
+                curr = curr->forward[i];
+            }
+            update[i] = curr;
+        }
+        int lv = randomLevel();
+        level = max(level, lv);
+        SkiplistNode *newNode = new SkiplistNode(num, lv);
+        for (int i = 0; i < lv; i++) {
+            /* 对第 i 层的状态进行更新，将当前元素的 forward 指向新的节点 */
+            newNode->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = newNode;
+        }
+    }
+
+    bool erase(int num) {
+        vector<SkiplistNode *> update(MAX_LEVEL, nullptr);
+        SkiplistNode *curr = this->head;
+        for (int i = level - 1; i >= 0; i--) {
+            /* 找到第 i 层小于且最接近 num 的元素*/
+            while (curr->forward[i] && curr->forward[i]->val < num) {
+                curr = curr->forward[i];
+            }
+            update[i] = curr;
+        }
+        curr = curr->forward[0];
+        /* 如果值不存在则返回 false */
+        if (!curr || curr->val != num) {
+            return false;
+        }
+        for (int i = 0; i < level; i++) {
+            if (update[i]->forward[i] != curr) {
+                break;
+            }
+            /* 对第 i 层的状态进行更新，将 forward 指向被删除节点的下一跳 */
+            update[i]->forward[i] = curr->forward[i];
+        }
+        delete curr;
+        /* 更新当前的 level */
+        while (level > 1 && head->forward[level - 1] == nullptr) {
+            level--;
+        }
+        return true;
+    }
+
+    int randomLevel() {
+        int lv = 1;
+        /* 随机生成 lv */
+        while (dis(gen) < P_FACTOR && lv < MAX_LEVEL) {
+            lv++;
+        }
+        return lv;
+    }
+};
+
+class Node{
+public:
+	Node(int val):val(val){
+		memset(next,0,sizeof(next));
+	}
+	Node* next[32];
+	int val;
+};
+class Skiplist {
+public:
+    Skiplist() {
+
+    }
+    
+    bool search(int target) {
+
+    }
+    
+    void add(int num) {
+
+    }
+    
+    bool erase(int num) {
+
+    }
+private:
+	
+};
