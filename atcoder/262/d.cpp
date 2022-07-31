198771a895491acd77bbb6d0db2278c34121eba8
wangqc12138 <472018166@qq.com>
2022-07-31 22:45:14 +0800

    20220731

---
+++ atcoder/262/d.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 200007;
+const int MOD = 1000000007;
+#define bit(x,i)(((x)>>(i))&1)
+using ll=long long;
+using pii=pair<int,int>;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int m=2;m<=n;m++){
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[arr[i]%m]++;
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    freopen("./test.txt","r",stdin);
+    solve();
+    return 0;
+}
