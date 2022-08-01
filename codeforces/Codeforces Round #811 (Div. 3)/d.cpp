62a83a95a90696f53fde3681fe9e33affa65acc1
wangqc12138 <472018166@qq.com>
2022-08-02 00:07:37 +0800

    20220802

--- codeforces/Codeforces Round #811 (Div. 3)/d.cpp
+++ codeforces/Codeforces Round #811 (Div. 3)/d.cpp
@@ -6,7 +6,27 @@ const int MOD = 1000000007;
 using ll=long long;
 using pii=pair<int,int>;
 void solve(){
-
+    string target;
+    int n;
+    cin>>target>>n;
+    // vector<int> temp(target.size());
+    set<pii> st;
+    map<pii,int> mp;
+    for(int i=0;i<n;i++){
+        string x;
+        cin>>x;
+        int k=target.find(x);
+        while(k>=0){
+            st.emplace(k,k+x.size());
+            mp[pii(k,k+x.size())]=i;
+            k=target.find(x,k+x.size());
+        }
+    }
+    vector<pii> temp(st.begin(),st.end());
+    int a=temp[0].first,b=temp[0].second;
+    for(int i=1;i<temp.size();i++){
+        if()
+    }
 }
 int main(){
     ios::sync_with_stdio(false);
