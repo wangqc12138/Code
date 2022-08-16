9ca8757fb98854d9245b4714690b0beb8d6207e5
wangqc12138 <472018166@qq.com>
2022-08-16 23:17:38 +0800

    20220810

---
+++ codeforces/Codeforces Round #814/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 200007;
+const int MOD = 1000000007;
+#define bit(x,i)(((x)>>(i))&1)
+#define no {puts("No") ; return ;}
+#define yes {puts("Yes") ; return ;}
+using ll=long long;
+using pii=pair<int,int>;
+void solve(){
+    int m,n;
+    cin>>m>>n;
+    if((m+n-1)%2==0){
+        cout<<"Burenka\n";
+    }else{
+        cout<<"Tonya\n";
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    freopen("./test.txt","r",stdin);
+    int tt;cin>>tt;for(int i=0;i<tt;i++){solve();}
+    return 0;
+}
