11fc2248abd1e8fe38a64a15b16e8c8f15bd4496
wangqc12138 <472018166@qq.com>
2022-11-14 16:03:04 +0800

    20221114

--- test/test.cpp
+++ test/test.cpp
@@ -2,33 +2,51 @@
 using namespace std;
 const int MAX = 200007;
 const int MOD = 1000000007;
-#define bit(x,i)(((x)>>(i))&1)
-#define no {puts("No") ; return ;}
-#define yes {puts("Yes") ; return ;}
-using ll=long long;
-using pii=pair<int,int>;
-void solve(){
-    int n;
-    string str;
-    cin>>n>>str;
-    string ans="";
-    for(int i=n-1;i>=0;i--){
-        if(str[i]=='0'){
-            string temp=str.substr(i-2,2);
-            ans+='a'+stoi(temp)-1;
-            i-=2;
-        }else{
-            ans+='a'+str[i]-'0'-1;
-        }
-    }
-    reverse(ans.begin(),ans.end());
-    cout<<ans<<endl;
+#define bit(x, i) (((x) >> (i)) & 1)
+#define no          \
+	{               \
+		puts("No"); \
+		return;     \
+	}
+#define yes          \
+	{                \
+		puts("Yes"); \
+		return;      \
+	}
+using ll = long long;
+using pii = pair<int, int>;
+void solve()
+{
+	int n;
+	string str;
+	cin >> n >> str;
+	string ans = "";
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (str[i] == '0')
+		{
+			string temp = str.substr(i - 2, 2);
+			ans += 'a' + stoi(temp) - 1;
+			i -= 2;
+		}
+		else
+		{
+			ans += 'a' + str[i] - '0' - 1;
+		}
+	}
+	reverse(ans.begin(), ans.end());
+	cout << ans << endl;
 }
-int main(){
-    ios::sync_with_stdio(false);
+int main()
+{
+	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-    freopen("./test.txt","r",stdin);
-    int tt;cin>>tt;for(int i=0;i<tt;i++){solve();}
-    return 0;
-	
+	freopen("./test.txt", "r", stdin);
+	int tt;
+	cin >> tt;
+	for (int i = 0; i < tt; i++)
+	{
+		solve();
+	}
+	return 0;
 }
