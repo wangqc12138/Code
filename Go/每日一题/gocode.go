fd29219f3a75b249ae1a1cdd8aca989971859abc
wangqc12138 <472018166@qq.com>
2022-12-15 17:11:06 +0800

    20221215

--- Go/每日一题/gocode.go
+++ Go/每日一题/gocode.go
@@ -1,6 +1,9 @@
 package main
 
-import "sort"
+import (
+	"sort"
+	"strconv"
+)
 
 /*
 有时候人们会用重复写一些字母来表示额外的感受，比如 "hello" -> "heeellooo", "hi" -> "hiii"。我们将相邻字母都相同的一串字符定义为相同字母组，例如："h", "eee", "ll", "ooo"。
@@ -371,5 +374,38 @@ func distanceLimitedPathsExist(n int, edgeList [][]int, queries [][]int) []bool 
 		}
 		res[vec[3]] = isSameSet(vec[0], vec[1])
 	}
+	return res
+}
+
+/*
+给你一个由小写字母组成的字符串 s ，以及一个整数 k 。
+
+首先，用字母在字母表中的位置替换该字母，将 s 转化 为一个整数（也就是，'a' 用 1 替换，'b' 用 2 替换，... 'z' 用 26 替换）。接着，将整数 转换 为其 各位数字之和 。共重复 转换 操作 k 次 。
+
+例如，如果 s = "zbax" 且 k = 2 ，那么执行下述步骤后得到的结果是整数 8 ：
+
+    转化："zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+    转换 #1：262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+    转换 #2：17 ➝ 1 + 7 ➝ 8
+
+返回执行上述操作后得到的结果整数。
+1945
+*/
+func getLucky(s string, k int) int {
+	for i := 0; i < k; i++ {
+		t := 0
+		for _, ch := range s {
+			if i == 0 {
+				temp := int(ch - 'a' + 1)
+				t += temp % 10
+				t += temp / 10
+			} else {
+				t += int(ch - '0')
+			}
+
+		}
+		s = strconv.Itoa(t)
+	}
+	res, _ := strconv.Atoi(s)
 	return res
 }
