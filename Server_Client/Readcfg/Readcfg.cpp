6491bb4b6a2a2ffe67802d93061c4ca34455739e
wangqc12138 <472018166@qq.com>
2023-06-01 10:41:40 +0800

    20230601

--- Server_Client/Readcfg/Readcfg.cpp
+++
@@ -1,8 +0,0 @@
-#include "Readcfg.h"
-int readcfg(const char* cfgname){
-	FILE* fd;
-	fd=fopen(cfgname,"r");
-	while(){
-
-	}
-}
--- Server_Client/Readcfg/Readcfg.h
+++
@@ -1,8 +0,0 @@
-#ifndef READ_CFG_H
-#define READ_CFG_H
-#include "common.h"
-int readcfg(const char* cfgname);
-class Cfg{
-
-};
-#endif
